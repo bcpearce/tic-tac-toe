251e77cfa22fab4114de3a0a3e6446ad49d160d8
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-25 00:00:11 -0400

    added test for updating the board

--- test/game_spec.rb
+++ test/game_spec.rb
@@ -10,17 +10,31 @@ describe "Game" do
   it { should respond_to("board") }
   it { should respond_to("players") }
   it { should respond_to("add_player") }
+  it { should respond_to("update_board") }
 
   describe "players" do
     
-    before do 
-      @game.add_player('X')
-      @game.add_player('O')
-    end
-   
-    subject { @game.players }
+    describe "it should allow adding players to the game" do
+      before do 
+        @game.add_player('X')
+        @game.add_player('O')
+      end
+     
+      subject { @game.players }
 
-    it { should have(2).items }
+      it { should have(2).items }
+    end
   end
+
+  describe "it should allow updating" do
+    before do
+      move = { 'X' => 5 } 
+      @game.update_board(move)
+    end
+
+    its("board[2,2]") { should eq('X') }
+
+  end
+
 
 end
