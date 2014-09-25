ed88e9b4e8cb6273001fd148296ff3b0513f0dee
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-24 23:20:33 -0400

    added test for player symbol and move making

--- lib/player.rb
+++ lib/player.rb
@@ -2,4 +2,12 @@ class Player
   def initialize(symbol)
     @symbol = symbol
   end
+
+  def symbol
+    @symbol
+  end
+
+  def make_move(position)
+    { @symbol => position }
+  end
 end
--- test/player_spec.rb
+++ test/player_spec.rb
@@ -1,1 +1,15 @@
 require_relative '../lib/player'
+
+describe "Player" do 
+
+  before { @player = Player.new('X') }
+
+  subject { @player }
+
+  it { should respond_to("symbol") }
+  it { should respond_to("make_move") }
+  
+  describe "symbol" do
+    its("symbol") { should eq('X') }
+  end
+end
