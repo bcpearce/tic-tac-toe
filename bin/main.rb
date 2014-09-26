df6fe8b66cb6bd2a17ead2a3a048694ef3e2c22c
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-25 23:01:58 -0400

    added game flow to 'main.rb'

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,44 @@
 require_relative '../lib/game.rb'
 
-game = Game.new
+def move(player)
+  puts "\nPlayer #{player.symbol}, enter a position:"
+  player.make_move(gets.chomp.to_i)
+end
 
-game.display_board
+@game = Game.new
 
-player1 = game.add_player('X')
-player2 = game.add_player('O')
+@game.display_board
 
-puts "Player #{player1.symbol}, enter a position:"
+@player1 = @game.add_player('X')
+@player2 = @game.add_player('O')
+
+while @game.open_positions?
+  current_player ||= @player1
+
+  unless @game.update_board(move(current_player))
+    puts "Invalid move, try again."
+    next
+  end
+  @game.display_board
+
+  if @game.is_winner?(current_player)
+    puts "\nYou Win!\n"
+    break
+  elsif @game.is_draw?
+    puts "\nCat's Game...\n"
+    break
+  end
+
+  if current_player == @player1
+    current_player = @player2
+  elsif current_player == @player2
+    current_player = @player1
+  end
+
+
+end
+
+
+
 
 
